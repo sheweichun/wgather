b2fa8ad00cbb5e32c2a5f2d6c6a3ad4f2569aa9a
洛丹 <weichun.swc@alibaba-inc.com>
2018-09-19 11:28:02 +0800

    修复gather-core目录错误问题

--- packages/gather-core/lib/util/config.js
+++ packages/gather-core/lib/util/config.js
@@ -1,11 +1,10 @@
 
 const path = require('path');
 const homeDir = require('os').homedir();
-const PLUGIN_PATH = path.join(homeDir, '.gather');
+
+const PLUGIN_PATH = path.join(homeDir, '.wgather');
 
 const fs = require('fs');
-
-
 
 
 const formatBoolean = (value) => {
@@ -18,37 +17,37 @@ const formatBoolean = (value) => {
   return value;
 };
 
-module.exports = (name,attrName,defaultConfig={})=>{
-    const filePath = path.join(PLUGIN_PATH, name);
-    const requireFile = () => require(filePath);
-    return {
-        init() {
-            if (!fs.existsSync(filePath)) {
-                fs.writeFileSync(filePath, JSON.stringify({[attrName]:defaultConfig}, null, 2));
-                return;
-            }
-            let config = requireFile();
-            if(!config[attrName]){
-                config[attrName] = defaultConfig;
-                fs.writeFileSync(filePath, JSON.stringify(config, null, 2));
-            }
-        },
-        get(option) {
-            if (!option) {
-                return requireFile();
-            }
-            return requireFile()[attrName][option];
-        },
-        set(option, value) {
-            const config = requireFile();
-            config[attrName][option] = formatBoolean(value);
-            fs.writeFileSync(filePath, JSON.stringify(config, null, 2));
-            return true;
+module.exports = (name, attrName, defaultConfig = {}) => {
+  const filePath = path.join(PLUGIN_PATH, name);
+  const requireFile = () => require(filePath);
+  return {
+    init() {
+      if (!fs.existsSync(filePath)) {
+        fs.writeFileSync(filePath, JSON.stringify({ [attrName]: defaultConfig }, null, 2));
+        return;
+      }
+      const config = requireFile();
+      if (!config[attrName]) {
+        config[attrName] = defaultConfig;
+        fs.writeFileSync(filePath, JSON.stringify(config, null, 2));
+      }
+    },
+    get(option) {
+      if (!option) {
+        return requireFile();
+      }
+      return requireFile()[attrName][option];
+    },
+    set(option, value) {
+      const config = requireFile();
+      config[attrName][option] = formatBoolean(value);
+      fs.writeFileSync(filePath, JSON.stringify(config, null, 2));
+      return true;
             // if (Object.prototype.hasOwnProperty.call(defaultConfig, option)) {
-                
+
             // }
             // return false;
-        },
-    }
+    },
+  };
 };
 
